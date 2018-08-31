30c5cf66b880db652fa9fda10e0a514c17e485d2
moonyoungJo <jomy9649@gmail.com>
2018-08-31 18:27:55 +0900

    Create project

---
+++ AlgorithmTraining/src/programmers/level2/Country124.java
@@ -0,0 +1,13 @@
+package programmers.level2;
+
+/*
+ * 124 나라의 숫자
+ * 
+ */
+
+public class Country124 {
+	public String solution(int n) {
+		String answer = "";
+		return answer;
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Algorithm trainning for recruit
